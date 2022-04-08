d1642e0d0d01d575103354a4f11079598412c695
Bret Edwards <sliverman69@gmail.com>
2022-04-07 22:43:49 -0700

    shortVolumeAnalyzer: re-work the design for handling input dates and re-do struct for tickers creation and management

--- cmd/shortVolumeAnalyzer.go
+++ cmd/shortVolumeAnalyzer.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -13,35 +12,29 @@ import (
 )
 
 const (
-	DEFAULT_TIME_FORMAT     = "2006-01-02"
-	DEFAULT_TIME_URL_STRING = "20060102"
+	DEFAULT_TIME_FORMAT           = "2006-01-02"
+	DEFAULT_TIME_URL_STRING       = "20060102"
+	HOURS_PER_DAY           int64 = 24
 )
 
 var (
-	startDate      string
-	endDate        string
-	singleDate     string
-	trailingDays   int64
-	inputURLPrefix string = "https://cdn.finra.org/equity/regsho/daily/CNMSshvol"
-	inputURLPost   string = ".txt"
+	defaultDays     int64
+	trailingDays    int64
+	startDateString string
+	endDateString   string
+	inputURLPrefix  string = "https://cdn.finra.org/equity/regsho/daily/CNMSshvol"
+	inputURLPost    string = ".txt"
 )
 
 func init() {
-	flag.StringVar(&startDate, "start", "2021-01-01",
-		"enter a date string in yyyy-mm-dd format for the day to start retrieving records from. Default: 2021-01-01.")
-	flag.StringVar(&startDate, "s", "2021-01-01",
-		"enter a date string in yyyy-mm-dd format for the day to start retrieving records from. Default: 2021-01-01.")
-	flag.StringVar(&endDate, "end", time.Now().Format("2006-01-02"),
-		"enter a date string in yyyy-mm-dd format to stop retrieving records from. Default: Today's date.")
-	flag.StringVar(&endDate, "e", time.Now().Format("2006-01-02"),
-		"enter a date string in yyyy-mm-dd format to stop retrieving records from. Default: Today's date.")
-	flag.StringVar(&singleDate, "one-day", time.Now().Format("2006-01-02"),
-		"enter a single date string in yyyy-mm-dd format to retrieve a single day of Finra Data. Default: Today's date.")
-	flag.StringVar(&singleDate, "o", time.Now().Format("2006-01-02"),
-		"enter a single date string in yyyy-mm-dd format to retrieve a single day of Finra Data. Default: Today's date.")
+	flag.Int64Var(&defaultDays, "days", 30, "enter a number of days to go back and retrieve records from today's date (calendar days). Default: 30.")
+	flag.Int64Var(&defaultDays, "d", 30, "enter a number of days to go back and retrieve records from today's date (calendar days). Default: 30.")
+	flag.StringVar(&startDateString, "start", "NA", "enter a date in format: -start 'YYYY-mm-dd'. Default NA.")
+	flag.StringVar(&endDateString, "end", "NA", "enter a date in format: -start 'YYYY-mm-dd'. Default NA.")
 }
 
-type dailyShortData struct {
+//DailyShortData contains the data for a single symbol for a single day of data.
+type DailyShortData struct {
 	Symbol                string    `json:symbol`                //provided
 	TradingDate           time.Time `json:timestamp`             //provided
 	TradingDayString      string    `json:dateString`            //provided
@@ -55,115 +48,186 @@ type dailyShortData struct {
 	BuyVol                float64   `json:buyVol`                //calculated
 }
 
-type shortData struct {
-	StockSymbol                string         `json:symbol`
-	TradingDate                string         `json:dateString`
-	ShortVolumeData            dailyShortData `json:finraShortVolumeData`
-	TotalShortVol              float64        `json:totalShortVol`
-	TotalExemptShortVol        float64        `json:totalExemptShortVol`
-	TotalBuyVol                float64        `json:totalBuyVol`
-	TotalSharesShort           float64        `json:totalSharesShort`
-	TotalVol                   float64        `json:totalVol`
-	TotalBuyVolPercent         float64        `json:totalBuyVolPercent`
-	TotalExemptShortVolPercent float64        `json:totalExemptShortVolPercent`
-	TotalShortInterestPercent  float64        `json:totalShortInterestPercent`
+type ShortData struct {
+	StockSymbol                string                    `json:symbol`
+	ShortVolumeData            map[string]DailyShortData `json:finraShortVolumeData` //string representation of trade date in form yyyymmdd
+	TotalShortVol              float64                   `json:totalShortVol`
+	TotalExemptShortVol        float64                   `json:totalExemptShortVol`
+	TotalBuyVol                float64                   `json:totalBuyVol`
+	TotalSharesShort           float64                   `json:totalSharesShort`
+	TotalVol                   float64                   `json:totalVol`
+	TotalBuyVolPercent         float64                   `json:totalBuyVolPercent`
+	TotalExemptShortVolPercent float64                   `json:totalExemptShortVolPercent`
+	TotalShortInterestPercent  float64                   `json:totalShortInterestPercent`
+	TotalSharesOutstanding     float64                   `json:totalSharesOutstanding`
+	TotalFloat                 float64                   `json:totalFloat`
+}
+
+//instantiate a new array of size N to contain all the DailyShortData structs of specific size so that you can end up with
+func NewDailyShortDataMap(numElems int) (dailyShortDataMapPtr *map[string]DailyShortData) {
+	dailyShortDataMap := make(map[string]DailyShortData, numElems)
+	return &dailyShortDataMap
+}
+
+func MakeShortDataMap() (shortDataMap *map[string]ShortData) {
+	shortDataList := make(map[string]ShortData)
+	return &shortDataList
+}
+
+func MakeNewShortData(symbol string, numElems int) (newShortDataElemPtr *ShortData) {
+	newShortDataElemPtr = &ShortData{
+		StockSymbol:     symbol,
+		ShortVolumeData: *NewDailyShortDataMap(numElems),
+	}
+	return
+}
+
+//simple function to check if a flag was passed in at the command line
+func isFlagPassed(name string) bool {
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
 }
 
 //fetchStockVolumeData grabs the data from Finra and returns a struct of type shortData containing all
 //the data in the finra daily volume txt web pages
-func fetchStockVolumeData(url string) (finraShortData []shortData, err error) {
+func fetchStockVolumeData(url string, finraShortDataMap *map[string]ShortData) (err error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return []shortData{}, err
+		return err
 	}
 	defer resp.Body.Close()
 
 	responseBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("error retrieving responseBody")
-		return []shortData{}, err
+		return err
 	}
-	respBodyBytes := strings.ReplaceAll(string(responseBody), "|", ",")
+	respBodyBytes := strings.ReplaceAll(string(responseBody), ",", ";")
+	respBodyBytes = strings.ReplaceAll(respBodyBytes, "|", ",")
 	respBodyBytesSlice := strings.Split(respBodyBytes, "\n")
 	respBodyBytes = strings.Join(respBodyBytesSlice[0:len(respBodyBytesSlice)-2], "\n")
 
 	var columnMapping = make(map[string]int)
 	for _, line := range strings.Split(respBodyBytes, "\n") {
+		line = strings.Trim(line, "\r") //remove carriage returns, which were polluting the "Market" field in the line data
+
+		//If there is not already a key mapping for the Columns, Create it if it's the first line of the csv-style formatting
 		if strings.Contains(strings.ToLower(line), "symbol") {
 			for index, key := range strings.Split(line, ",") {
 				columnMapping[key] = index
 			}
 			fmt.Printf("column Mapping:\n%v", columnMapping)
+
+			//Bulk of logic to create the struct is here.
 		} else {
 			tradeDate, err := time.Parse("20060102", strings.Split(line, ",")[columnMapping["Date"]])
 			if err != nil {
 				fmt.Printf("unable to parse trade date: %v", err)
-				return []shortData{}, err
+				return err
 			}
 			shortVolume, err := strconv.ParseFloat(strings.Split(line, ",")[columnMapping["ShortVolume"]], 64)
 			if err != nil {
 				fmt.Printf("unable to parse Short Volume: %v", err)
-				return []shortData{}, err
+				return err
 			}
 			shortExemptVolume, err := strconv.ParseFloat(strings.Split(line, ",")[columnMapping["ShortExemptVolume"]], 64)
 			if err != nil {
 				fmt.Printf("unable to parse Short Exempt Volume: %v", err)
-				return []shortData{}, err
+				return err
 			}
 			totalTradeVolume, err := strconv.ParseFloat(strings.Split(line, ",")[columnMapping["TotalVolume"]], 64)
 			if err != nil {
 				fmt.Printf("unable to parse Total Trade Volume: %v", err)
-				return []shortData{}, err
+				return err
 			}
-			symbolShortData := dailyShortData{
+			marketArray := strings.Split((strings.Split(line, ",")[columnMapping["Market"]]), ";")
+			symbolShortData := DailyShortData{
 				Symbol:           strings.Split(line, ",")[columnMapping["Symbol"]],
 				TradingDate:      tradeDate,
 				TradingDayString: tradeDate.Format(DEFAULT_TIME_FORMAT),
 				ShortVol:         shortVolume,
 				ShortExemptVol:   shortExemptVolume,
 				TotalVolume:      totalTradeVolume,
-				Market:           strings.Split(strings.Split(line, ",")[columnMapping["Market"]], ","),
+				Market:           marketArray,
 			}
-			symbolShortData.ShortVolPercent = (symbolShortData.TotalVolume - symbolShortData.ShortVol) / symbolShortData.TotalVolume * 100
-			symbolShortData.ShortExemptVolPercent = (symbolShortData.TotalVolume - symbolShortData.ShortExemptVol) / symbolShortData.TotalVolume * 100
+			symbolShortData.ShortVolPercent = (symbolShortData.ShortVol) / symbolShortData.TotalVolume * 100
+			symbolShortData.ShortExemptVolPercent = (symbolShortData.ShortExemptVol) / symbolShortData.TotalVolume * 100
 			symbolShortData.BuyVol = symbolShortData.TotalVolume - symbolShortData.ShortVol
-			symbolShortData.BuyVolPercent = (symbolShortData.TotalVolume - symbolShortData.BuyVol) / symbolShortData.TotalVolume * 100
-			finraShortData = append(finraShortData,
-				shortData{
-					StockSymbol:     symbolShortData.Symbol,
-					ShortVolumeData: symbolShortData,
-				})
+			symbolShortData.BuyVolPercent = (symbolShortData.BuyVol) / symbolShortData.TotalVolume * 100
 		}
 	}
-	return finraShortData, err
+	return err
 }
 
+//worker function to coordinate fetching data and adding it to the map of tickers.
+func worker(tickers *map[string]ShortData, start, end time.Time, deltaDays int64) (err error) {
+	/*
+	* Retrieve a day of data and store it in the map.
+	* Handle page retrieve errors on days that don't trade
+	* Tabulate total short shares outstanding. If a day exists where buy vol exceeds 50% of trading (ie. delta positive shares),
+	* subtract those from the existing shares short.
+	*
+	* tickers: the pointer to the map of ticker symbols to track short interest data on.
+	* deltaDays: number of days that are to be retrieved back on the calendar.
+	* start: a Time var indicating the first trading day to retrieve.
+	* end: a Time var indicating the last trading day to retrieve.
+	*
+	 */
+
+	for i := 0; i < int(deltaDays); i++ {
+		retrieveDate := start.Add(time.Duration(int64(i * int(time.Hour) * 24)))
+		fmt.Printf("date of retrieval is: %v\n", retrieveDate.Format("20060102"))
+	}
+
+	return nil
+}
+
+//Retrieve short vol data from FINRA, organize the data, and then present data based on highest short interest.
 func main() {
+
+	var (
+		startDate, endDate time.Time
+		numDays            int64
+	)
 
 	flag.Parse()
 
-	//determine whether or not to select single date of data, start and end date, or range of dates based on input flags.
+	//Need to check for existence of both walkbaack days and start/end dates. If both exist, either error out or choose one to have preceedence over the other.
 
-	inputDate, err := time.Parse(DEFAULT_TIME_FORMAT, singleDate)
-	if err != nil {
-		fmt.Printf("Unable to parse single date input. Error message:\n%s", err)
-		os.Exit(1)
-	}
-	inputDateString := inputDate.Format(DEFAULT_TIME_URL_STRING)
-
-	inputURL := inputURLPrefix + inputDateString + inputURLPost
-	finraData, err := fetchStockVolumeData(inputURL)
-	if err != nil {
-		fmt.Printf("Error retrieving stock volume data from finra: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("Finra data returned:")
-	for _, stockSymbol := range finraData {
-		fmt.Printf("%s:\n", stockSymbol.StockSymbol)
-		jsonData, err := json.MarshalIndent(stockSymbol, "", "    ")
+	//check if the walk-back of days is set correctly.
+	if isFlagPassed("days") || isFlagPassed("d") {
+		startDate = time.Now().Add(time.Duration(-1*defaultDays*24) * time.Hour)
+		endDate = time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 0, 0, 0, 0, time.UTC)
+		numDays = defaultDays
+	} else {
+		startDate, err := time.Parse(DEFAULT_TIME_FORMAT, startDateString)
 		if err != nil {
-			fmt.Printf("Unable to Marshal JSON data")
+			fmt.Printf("Error converting start Date into a date format. Required format: YYYY-mm-dd, where Y is year, m is month, and d is day. Error msg:\n %v", err)
+			os.Exit(1)
 		}
-		fmt.Printf(string(jsonData))
+		endDate, err := time.Parse(DEFAULT_TIME_FORMAT, endDateString)
+		if err != nil {
+			fmt.Printf("Error converting end Date into a date format. Required format: YYYY-mm-dd, where Y is year, m is month, and d is day. Error msg:\n %v", err)
+			os.Exit(1)
+		}
+		numDays = int64(endDate.Sub(startDate).Hours()) / HOURS_PER_DAY
 	}
+
+	//create map of tickers
+	tickerMap := make(map[string]ShortData)
+
+	//kick off the function that will do most of the work.
+	err := worker(&tickerMap, startDate, endDate, numDays)
+	if err != nil {
+		fmt.Printf("Error running main worker. Error is: %v", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Completed tasks. Exiting Cleanly.")
+	return
 }
